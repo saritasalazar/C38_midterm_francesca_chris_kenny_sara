24d6f86ffcc320df300586ae226a06ec831843bb
Christopher Falcon <chrisfalcon59@hotmail.com>
2020-08-01 17:51:45 -0400

    Nav Bar

--- client/src/components/Menulist.js
+++ client/src/components/Menulist.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Container from "react-bootstrap/Container";
 import SearchDrinks from './SearchDrinks';
 import NavBar from './NavBar'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AboutUs from './AboutUs'
 
 const Menulist = () => {
     return (
-    <>
+    <Router>
         <NavBar />
         <Container>
-            <SearchDrinks />
+            <Switch>
+                <Route path = "/AboutUs" component = {AboutUs}/>
+                {/* <Route path = "/" component = {SearchDrinks}/> */}
+            </Switch>
+            
         </Container>
-    </>
+    </Router>
     );
 };
 export default Menulist;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,13 +5,19 @@ const NavBar = () => {
     return(
         <Nav style={{ backgroundColor: "tomato" }} defaultActiveKey="/home" as="ul">
             <Nav.Item as ="li">
-                <Nav.Link style={{ color: "#fff" }} href="/random">
+                <Nav.Link style={{ color: "#fff" }} href="/">
+                    <strong>Home</strong>
+                </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as ="li">
+                <Nav.Link style={{ color: "#fff" }} href="/random" eventKey="link-1">
                     <strong>Drink of the Day</strong>
                 </Nav.Link>
             </Nav.Item>
 
             <Nav.Item as ="li">
-                <Nav.Link style={{ color: "#fff" }} href="/about">
+                <Nav.Link style={{ color: "#fff" }} href="/AboutUs" eventKey="link-2">
                     <strong>About Us</strong>
                 </Nav.Link>
             </Nav.Item>
